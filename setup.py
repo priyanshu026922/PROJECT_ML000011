a03e493226a78e479161923b27a7aba5306e353f
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-12-19 02:11:42 +0530

    add setup.py requiremnts.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+  requirements=[]
+  with open(file_path)as file_obj:
+    requirements=file_obj.readlines()
+    requirements=[req.replace("\n","") for req in requirements]
+    
+    if '-e .' in requirements:
+      requirements.remove('-e .')
+
+
+
+
+setup(
+   name='MLproject',
+   version='0.0.1',
+   author='Priyanshu',
+   author_email='priyanshuranjan7856@gmail.com',
+   packages=find_packages(),
+   install_requires=get_requirements('requirements.txt')
+)
